14baba0b863f00896c0f5eecb13a83a021ca438e
chozen <moustafa.eldehni@hotmail.com>
2025-05-20 12:42:10 +0200

    partie plan css fini

--- src/components/plan/plan.jsx
+++ src/components/plan/plan.jsx
@@ -2,10 +2,11 @@ import "./plan.css"
 import arcade from "../../../public/img/arcade.svg"
 import advanced from "../../../public/img/advenced.svg"
 import pro from "../../../public/img/pro.svg"
+import { useState } from "react"
 
 export default function Plan({yearly, setYearly}){
 
-
+    const [change, setChange] = useState(false)
 
     return(
         <div className="plan">
@@ -14,17 +15,35 @@ export default function Plan({yearly, setYearly}){
                 <p>You have the option of monthly or yearly billing</p>
             </div>
             <div className="divPlans">
+
                 <div className="cardPlan">
                     <img className="cardImg" src={arcade} alt="" />
-                    <span>Arcade</span>
-                    <span>{yearly === false ? "$9/mo" : "$90/yr"}</span>
+
+                    <div className="cardPricing">
+                        <span>Arcade</span>
+                        <span>{yearly === false ? "$9/mo" : "$90/yr"}</span>
+                    </div>
+                </div>
+                <div className="cardPlan">
+                    <img className="cardImg" src={advanced} alt="" />
+                    <div className="cardPricing">
+                        <span>Arcade</span>
+                        <span>{yearly === false ? "$12/mo" : "$120/yr"}</span>
+                    </div>
+                </div>
+                <div className="cardPlan">
+                    <img className="cardImg" src={pro} alt="" />
+                    <div className="cardPricing">
+                        <span>Arcade</span>
+                        <span>{yearly === false ? "$15/mo" : "$150/yr"}</span>
+                    </div>
                 </div>
 
             </div>
             <div className="divSwitchPlan">
-                <span>Monthly</span>
-                <div className="switch"><div className="ball"></div></div>
-                <span>Yearly</span>
+                <span className={`${change === true ? "" : "planChosen"}`}>Monthly</span>
+                <div onClick={()=> {setChange(!change)}} className="switch"><div className={`ball ${change === true ? "on" : ""}`}></div></div>
+                <span className={`${change === true ? "planChosen" : ""}`}>Yearly</span>
             </div>
             <button onClick={()=> setStep("plan")} className="btnNext">Next Step</button>
             <span className="btnGoBack">Go Back</span>

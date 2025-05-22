a633020b4cb03100ab3176ab4a654595241c873c
chozen <moustafa.eldehni@hotmail.com>
2025-05-22 11:21:16 +0200

    Ajout du bonus clickable aux steps

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,31 +1,41 @@
 import "./sidebar.css"
 
-export default function Sidebar({dark, step, setStep}){
+export default function Sidebar({dark, step, setStep, valueNom, valueMail, valueTel, plan}){
+
+    const StepToPlan = () => {
+        !valueNom || !valueMail || !valueTel ? "" : setStep("plan")
+    }
+    const StepToAddons = () => {
+        !plan || !valueNom || !valueMail || !valueTel ? "" : setStep("addons")
+    }
+    const StepToSummary = () => {
+        !plan || !valueNom || !valueMail || !valueTel ? "" : setStep("summary")
+    }
 
     return(
         <div className={`sidebar ${dark ? "dark" : ""}`}>
-            <div className="opt">
+            <div onClick={()=> setStep("infos")} className={`opt ${!valueNom || !valueMail || !valueTel ? "" : "valide"}`}>
                 <div className={`optBubble ${step === "infos" ? "active" : ""}`}>1</div>
                 <div className="optText">
                     <span className="optSteps">STEP 1</span>
                     <span className="optTitle">YOUR INFOS</span>
                 </div>
             </div>
-            <div className="opt">
+            <div onClick={StepToPlan} className={`opt ${!valueNom || !valueMail || !valueTel ? "" : "valide"}`}>
                 <div className={`optBubble ${step === "plan" ? "active" : ""}`}>2</div>
                 <div className="optText">
                     <span className="optSteps">STEP 2</span>
                     <span className="optTitle">SELECT PLAN</span>
                 </div>
             </div>
-            <div className="opt">
+            <div onClick={StepToAddons} className={`opt ${!valueNom || !valueMail || !valueTel ? "" : "valide"}`}>
                 <div className={`optBubble ${step === "addons" ? "active" : ""}`}>3</div>
                 <div className="optText">
                     <span className="optSteps">STEP 3</span>
                     <span className="optTitle">ADD-ONS</span>
                 </div>
             </div>
-            <div className="opt">
+            <div onClick={StepToSummary} className={`opt ${!valueNom || !valueMail || !valueTel ? "" : "valide"}`}>
                 <div className={`optBubble ${step === "summary" ? "active" : ""}`}>4</div>
                 <div className="optText">
                     <span className="optSteps">STEP 4</span>
